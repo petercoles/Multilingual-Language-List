c3fb20f69250d085f26608cbc694b9d8adffc8eb
Peter Coles <peter@kuroiweb.com>
2019-09-16 16:17:08 +0100

    Update PHPUnit and bring test code into line.

--- tests/MixedLocaleTest.php
+++ tests/MixedLocaleTest.php
@@ -1,10 +1,12 @@
 <?php
 
-class MixedLocaleTest extends \PHPUnit_Framework_TestCase
+use PHPUnit\Framework\TestCase;
+
+class MixedLocaleTest extends TestCase
 {
     protected $maker;
         
-    public function setUp()
+    public function setUp(): void
     {
         $this->maker = new PeterColes\Languages\Maker;
     }
